a86ea7240833f69479b13e7d287ed63d4c4a4545
Robson Ferreira <tech.rob@icloud.com>
2021-09-27 16:58:16 -0400

    "files moved, deleted, code refactored"

--- 02/02_06/script.py
+++
@@ -1,2 +0,0 @@
-
-def find_most_common_word():
--- 03/03_06/begin/script.py
+++
@@ -1,4 +0,0 @@
-
-def generate_dictionary(monster_name, title, price, scariness):
-	return {'monster_name': monster_name, 'title': title, 
-	'price': price, 'scariness': scariness}
